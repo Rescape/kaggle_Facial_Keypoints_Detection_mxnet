198b8698647540b119d9adac79064a03fd89cf43
fengshikun <fengxiaoxiaokun@gmail.com>
2016-05-18 20:03:40 +0800

    add pool layer

--- symbol.py
+++ symbol.py
@@ -19,15 +19,15 @@ def get_symbol(output_dim = 30):
 	conv3 = mx.symbol.Convolution(
 		data=pool2, kernel=(2, 2), stride=(1, 1), num_filter=128, name="conv3")
 	relu3 = mx.symbol.Activation(data=bn3, act_type="relu", name="relu3")
-	pool3 = mx.symbol.Pooling(data=relu3, pool_type="max", kernel=(2, 2), stride=(2, 2), name="pool3")
+	# pool3 = mx.symbol.Pooling(data=relu3, pool_type="max", kernel=(2, 2), stride=(2, 2), name="pool3")
 
 	
 
-	# flatten concat
-	pool3_flatten = mx.symbol.Flatten(data=pool3, name="pool3_flatten")
+	# drop3
+	drop3 = mx.symbol.Flatten(data=relu3, name="drop3", p = 0.5)
 
 	# fc (fully connect)
-	fc1 = mx.symbol.FullyConnected(data=pool3_flatten, num_hidden=500, name="fc1")
+	fc1 = mx.symbol.FullyConnected(data=drop3, num_hidden=500, name="fc1")
 	fc2 = mx.symbol.FullyConnected(data=fc1, num_hidden=500, name="fc2")
 	fc3 = mx.symbol.FullyConnected(data=fc2, num_hidden=output_dim, name="fc3")
 	return fc3
